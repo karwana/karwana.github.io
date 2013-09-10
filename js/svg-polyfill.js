08997b818132357d5e74190afdfbd2f7a4f6ee0f
Matthew Caruana Galizia <mattcg@gmail.com>
2013-09-10 02:22:27 -0600

    Engrish

--- js/svg-polyfill.js
+++ js/svg-polyfill.js
@@ -4,7 +4,7 @@ document.body.addEventListener('error', function(event) {
 
 	if (img.src) {
 
-		// Switch source to PNG fallback for browser that don't support SVG.
+		// Switch source to PNG fallback for browsers that don't support SVG.
 		img.src = img.src.replace(/\.svg$/, '.png');
 	}
 }, true);
